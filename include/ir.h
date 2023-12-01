b5320ac844767872c1959545cc333201b2b3e889
thelllmike <sachinharshitha179@gmail.com>
2023-12-01 11:55:29 +0530

    ir fix

--- include/ir.h
+++ include/ir.h
@@ -1,149 +1,65 @@
-#include <iostream>
+#ifndef IR_H
+#define IR_H
+
 #include <string>
 #include <vector>
 #include <memory>
+#include <iostream>
 
-// Abstract class for any expression in the IR
 class Expression {
 public:
     virtual ~Expression() = default;
-    virtual int evaluate() const = 0;  // Pure virtual function for evaluation
-    virtual void print() const = 0;    // Pure virtual function for printing
+    virtual int evaluate() const = 0;
+    virtual void print() const = 0;
 };
 
-// Expression for integer literals
 class IntegerLiteral : public Expression {
 public:
     int value;
-
     explicit IntegerLiteral(int value) : value(value) {}
-    int evaluate() const override {
-        return value;
-    }
-    void print() const override {
-        std::cout << value;
-    }
+    int evaluate() const override;
+    void print() const override;
 };
 
-// Expression for variables (assuming you need this)
 class VariableExpression : public Expression {
 public:
     std::string name;
-
     explicit VariableExpression(std::string name) : name(std::move(name)) {}
-    int evaluate() const override {
-        // Implement variable lookup and return its value
-        return 0; // Replace with actual logic
-    }
-    void print() const override {
-        std::cout << name;
-    }
+    int evaluate() const override;
+    void print() const override;
 };
 
-// Binary operation expression
 class BinaryOperation : public Expression {
 public:
-    std::unique_ptr<Expression> left;
-    std::unique_ptr<Expression> right;
-    char op; // Operator: '+', '-', '*', '/'
-
+    std::unique_ptr<Expression> left, right;
+    char op;
     BinaryOperation(std::unique_ptr<Expression> left, std::unique_ptr<Expression> right, char op)
         : left(std::move(left)), right(std::move(right)), op(op) {}
-    int evaluate() const override {
-        int leftValue = left->evaluate();
-        int rightValue = right->evaluate();
-        switch (op) {
-            case '+': return leftValue + rightValue;
-            case '-': return leftValue - rightValue;
-            case '*': return leftValue * rightValue;
-            case '/': return leftValue / rightValue;
-            default:
-                throw std::runtime_error("Unknown operator");
-        }
-    }
-    void print() const override {
-        std::cout << "(";
-        left->print();
-        std::cout << " " << op << " ";
-        right->print();
-        std::cout << ")";
-    }
+    int evaluate() const override;
+    void print() const override;
 };
 
-// Base class for any IR instruction
 class Instruction {
 public:
     virtual ~Instruction() = default;
-    // Other methods for execution or visiting could be added here
+    virtual void execute() = 0;
 };
 
-// Assignment instruction
 class Assignment : public Instruction {
 public:
     std::string variableName;
     std::unique_ptr<Expression> expression;
-
     Assignment(std::string variableName, std::unique_ptr<Expression> expression)
         : variableName(std::move(variableName)), expression(std::move(expression)) {}
-    void print() const {
-        std::cout << variableName << " = ";
-        expression->print();
-        std::cout << ";" << std::endl;
-    }
+    void execute() override;
 };
 
-// Print instruction
 class Print : public Instruction {
 public:
     std::unique_ptr<Expression> expression;
-
     explicit Print(std::unique_ptr<Expression> expression)
         : expression(std::move(expression)) {}
-    void print() const {
-        std::cout << "print ";
-        expression->print();
-        std::cout << ";" << std::endl;
-    }
+    void execute() override;
 };
 
-// The entire IR for a program
-class IRProgram {
-public:
-    std::vector<std::unique_ptr<Instruction>> instructions;
-
-    // Add an instruction to the program
-    void addInstruction(std::unique_ptr<Instruction> instruction) {
-        instructions.push_back(std::move(instruction));
-    }
-
-    // Print all instructions in the program
-    void print() const {
-        for (const auto& instruction : instructions) {
-            instruction->print();
-        }
-    }
-};
-
-int main() {
-    // Create an IR program
-    IRProgram program;
-
-    // Create expressions and instructions
-    std::unique_ptr<Expression> expr1 = std::make_unique<IntegerLiteral>(42);
-    std::unique_ptr<Expression> expr2 = std::make_unique<IntegerLiteral>(7);
-    std::unique_ptr<Expression> expr3 = std::make_unique<BinaryOperation>(
-        std::move(expr1), std::move(expr2), '+'
-    );
-
-    std::unique_ptr<Instruction> assign = std::make_unique<Assignment>("result", std::move(expr3));
-    std::unique_ptr<Instruction> print = std::make_unique<Print>(std::make_unique<VariableExpression>("result"));
-
-    // Add instructions to the program
-    program.addInstruction(std::move(assign));
-    program.addInstruction(std::move(print));
-
-    // Print the entire IR program
-    program.print();
-
-    return 0;
-}
+#endif // IR_H
--- src/ir.cpp
+++ src/ir.cpp
@@ -1,71 +1,50 @@
 #include "ir.h"
-#include <iostream>
 
-
-// Implement the evaluate() and print() methods for IntegerLiteral
 int IntegerLiteral::evaluate() const {
-    // Placeholder implementation
-    // In a real scenario, this would return the integer value.
     return value;
 }
 
 void IntegerLiteral::print() const {
-    // This will print the integer value to the console.
     std::cout << value;
 }
 
-// Implement the evaluate() and print() methods for VariableExpression
 int VariableExpression::evaluate() const {
-    // Placeholder implementation
-    // In a real scenario, this might look up the variable's value in some sort of symbol table.
-    return 0; // Returning 0 as a placeholder
+    // Placeholder: Variable evaluation depends on your context (symbol table, etc.)
+    return 0;
 }
 
 void VariableExpression::print() const {
-    // This will print the variable's name to the console.
     std::cout << name;
 }
 
-// Implement the evaluate() and print() methods for BinaryOperation
 int BinaryOperation::evaluate() const {
-    // Placeholder implementation
-    // In a real scenario, this would perform the operation on the left and right operands.
-    // Here's a simple example:
     int leftVal = left->evaluate();
     int rightVal = right->evaluate();
-
     switch (op) {
         case '+': return leftVal + rightVal;
         case '-': return leftVal - rightVal;
         case '*': return leftVal * rightVal;
         case '/': return rightVal != 0 ? leftVal / rightVal : 0; // Guard against division by zero
-        default: return 0; // Or throw an exception for an unsupported operator
+        default: throw std::runtime_error("Unsupported operator");
     }
 }
 
 void BinaryOperation::print() const {
-    // This will print the binary operation in a "(left op right)" format.
-    std::cout << '(';
     left->print();
-    std::cout << ' ' << op << ' ';
+    std::cout << " " << op << " ";
     right->print();
-    std::cout << ')';
 }
 
-// Implement the print() method for Assignment
-void Assignment::print() const {
-    // This will print the assignment in a "variableName = expression;" format.
+void Assignment::execute() {
+    // Execute the assignment. This is a placeholder.
+    // In a real scenario, this would involve updating a symbol table or memory.
     std::cout << variableName << " = ";
     expression->print();
-    std::cout << ";\n";
+    std::cout << "\n";
 }
 
-// Implement the print() method for Print
-void Print::print() const {
-    // This will print the expression in a "print expression;" format.
-    std::cout << "print ";
+void Print::execute() {
+    // Execute the print instruction. This is a placeholder.
     expression->print();
-    std::cout << ";\n";
+    std::cout << "\n";
 }
-
-// Note: If the evaluate() method is also required for Assignment or Print, it would need to be declared and defined as well.
